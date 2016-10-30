5428c669bef86ffd0aee294a00c5919478b5102e
Kristine Francisco <kriskelfran@gmail.com>
2016-10-30 19:36:28 -0400

    hw

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,21 @@
+document.write('<p>' + 10*10 + '</p>');
+document.write('<h1>' + 100/10 + '</h1>');
+document.write(50-17);
+document.write(50+17);
+document.write('<p>Kristine Francisco</p>');
+
+var num1 = 10*10;
+
+document.write(num1);
+
+function hello () {
+  var greeting = 'hello!';
+
+  greeting = 'hello, world!';
+
+  alert(greeting);
+}
+
+hello();
+
+document.querySelector('#title', '#text-chunk').style.color='red';
