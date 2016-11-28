49838e83cc8380d39fb24795c584621c656de4af
Kristine Francisco <kriskelfran@gmail.com>
2016-11-28 16:35:07 -0500

    js update
    droppable changed depending on draggable id

--- final-project/js/main.js
+++ final-project/js/main.js
@@ -1,15 +1,23 @@
   $( function() {
-    $( ".ui-widget-content" ).draggable({ opacity: 0.7, helper: "clone" });
-    $( "#droppable" ).droppable({
+    $( '.ui-widget-content' ).draggable({ opacity: 0.7, helper: 'clone' });
+    $( '#droppable' ).droppable({
       drop: function( event, ui ) {
-            $(this).html( "DONUT CAT!");
+           if (ui.draggable.attr('id') == 'egg') { 
+            $(this).html( 'EGG CAT!');
+          } else if (ui.draggable.attr('id') == 'toast') {
+            $(this).html( 'TOAST CAT!');
+          } else if (ui.draggable.attr('id') == 'avocado') {
+            $(this).html( 'AVOCADO CAT!');
+          } else {
+            $(this).html( 'ORANGE JUICE CAT!');
+          }
       }
     });
   } );
 
-  // make food items draggable X
-  // make catghost droppable X
-  // change droppable depending on which draggable is dropped
-  //  - read which draggable is dragged (ie. #pizza, #icepop, etc.)
-  //  - droppable output "pizza img"(etc.) if "#pizza"(etc.) draggable is dropped
+  // make food items draggable [X]
+  // make catghost droppable [X]
+  // change droppable depending on which draggable is dropped 
+  //  - get id of draggable in play (ie. #pizza, #icepop, etc.) [X]
+  //  - droppable output "pizza img"(etc.) if "#pizza"(etc.) draggable is dropped [X]
 
